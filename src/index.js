edf20f44014e82cb8286b36d0f2965fd149a9fef
Alan Ma <34133975+Alan315123@users.noreply.github.com>
2024-06-21 21:31:33 +0800

    Update index.js

--- src/index.js
+++ src/index.js
@@ -7,16 +7,16 @@ const dockerHub = "https://registry-1.docker.io";
 
 const routes = {
   // production
-  "docker.libcuda.so": dockerHub,
-  "quay.libcuda.so": "https://quay.io",
-  "gcr.libcuda.so": "https://gcr.io",
-  "k8s-gcr.libcuda.so": "https://k8s.gcr.io",
-  "k8s.libcuda.so": "https://registry.k8s.io",
-  "ghcr.libcuda.so": "https://ghcr.io",
-  "cloudsmith.libcuda.so": "https://docker.cloudsmith.io",
+  "docker.alanm.top": dockerHub,
+  "quay.alanm.top": "https://quay.io",
+  "gcr.alanm.top": "https://gcr.io",
+  "k8s-gcr.alanm.top": "https://k8s.gcr.io",
+  "k8s.alanm.top": "https://registry.k8s.io",
+  "ghcr.alanm.top": "https://ghcr.io",
+  "cloudsmith.alanm.top": "https://docker.cloudsmith.io",
 
   // staging
-  "docker-staging.libcuda.so": dockerHub,
+  "docker-staging.alanm.top": dockerHub,
 };
 
 function routeByHosts(host) {
